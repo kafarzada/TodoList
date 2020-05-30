7e948ebf0f58d07a25aa03a1adb3766941615e48
kafarzada <52622175+kafarzada@users.noreply.github.com>
2020-05-30 15:43:52 +0700

    added new Project TodoList

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import TodoList from "./TodoList";
+
+
+function App() {
+    let task1 = [
+        {id: 1, title: "What to Learn", isDone: true},
+        {id: 2, title: "Song", isDone: true},
+        {id: 3, title: "Book", isDone: false}
+    ];
+
+    let task2 = [
+        {id: 1, title: "Hello World", isDone: false},
+        {id: 2, title: "I am Happy", isDone: true},
+        {id: 3, title: "YO", isDone: false}
+    ];
+
+
+    return (
+        <div className="App">
+            <TodoList title={"What to Learn"} tasks={task1}/>
+            <TodoList title={"Songs"} tasks={task2}/>
+        </div>
+    );
+}
+
+export default App;
